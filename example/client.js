c3424de982fc23f973b080724845bd2bf4d7576d
kazuya kawaguchi <kawakazu80@gmail.com>
2011-11-09 00:11:13 +0900

    Update client example.

--- example/client.js
+++ example/client.js
@@ -20,23 +20,23 @@ clinet.connect(/* { host: 'localhost', port: 20000, }, */function (err) { // on(
   }
 
   // general sample.
-  var add_task = client.do({ // return task.
+  client.do({ // return task.
     // ns: '/', // if namespace do not specific, default namespace '/'
     func: 'add', // function name.
     args: { // funciton arguments.
       a: 1,
       b: 2,
     },
-  });
-
-  // complete event.
-  add_task.on('complete', function (res) {
-    console.log('add complete : ' + res);
+  }, function (job) {
+    // complete event.
+    job.on('complete', function (res) {
+      console.log('add complete : ' + res);
+    });
   });
 
 
   // timeout sample.
-  var sub_task = lient.do({
+  client.do({
     ns: '/ns1/', // namespace.
     func: 'sub',
     args: {
@@ -44,51 +44,49 @@ clinet.connect(/* { host: 'localhost', port: 20000, }, */function (err) { // on(
       b: 2,
     },
     timeout: 5000,
-  });
-
-  // timeout event.
-  sub_task.on('timeout', function (code) { // TODO: code : 1 -> network? 2 -> task do not finsh?
-    console.log('sub timeout : ', code);
-  });
-
-  // complete event.
-  sub_task.on('complete', function (res) {
-    console.log('sub complete : ' + res);
+  }, function (job) {
+    // timeout event.
+    job.on('timeout', function (code) { // TODO: code : 1 -> network? 2 -> task do not finsh?
+      console.log('sub timeout : ', code);
+    });
+    // complete event.
+    job.on('complete', function (res) {
+      console.log('sub complete : ' + res);
+    });
   });
 
 
   // error sample.
-  var mul_task = client.do({
+  client.do({
     func: 'mul',
     args: {
       a: 2,
       //b: 1,
     }
-  });
-
-  // fail event.
-  mul_task.on('fail', function (err) {
-    console.log('mul fail : ' + err);
-  });
-
-  // complete event.
-  mul_task.on('complete', function (res) {
-    console.log('mul complete : ' + res);
+  }, function (job) {
+    // fail event.
+    job.on('fail', function (err) {
+      console.log('mul fail : ' + err);
+    });
+    // complete event.
+    job.on('complete', function (res) {
+      console.log('mul complete : ' + res);
+    });
   });
 
 
   // binary parameter sample.
   var image_buf = new Buffer(256 * 256); // image data.
-  var image_proc_task = client.do({
+  client.do({
     func: 'process_image',
     args: {
       image: image_buf,
     }
-  });
-
-  // complete event.
-  image_proc_task.on('complete', function (res) {
-    console.log('process_image : ' + res);
+  }, function (job) {
+    // complete event.
+    job.on('complete', function (res) {
+      console.log('process_image : ' + res);
+    });
   });
 
 }); // end of client.connect
@@ -99,7 +97,8 @@ process.on('exit', function (code, signal) {
   client.disconnect(function (err) { // on('disconnect', function () { ... });
     // error handling
     if (err) { 
-      // ...
+      // todo ...
+      return;
     }
     console.log('disconnect');
   });
