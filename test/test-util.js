6b0b78deb8034f3f26f04f38c2763e4135281153
kazuya kawaguchi <kawakazu80@gmail.com>
2011-11-29 00:28:20 +0900

    Extend the EventEmitter.

--- test/test-util.js
+++ test/test-util.js
@@ -12,6 +12,7 @@ var vows = require('vows');
 var assert = require('assert');
 var format = require('util').format;
 var util = require('../lib/util');
+var EventEmitter = require('../lib/util').EventEmitter;
 
 
 //
@@ -39,8 +40,7 @@ function withContext (func, args, ret) {
 }
 
 
-var suite = vows.describe('util.js tests');
-suite.addBatch({
+vows.describe('assert tests').addBatch({
   'true': withContext(util.assert, {
     condition: true, message: '',
   }, true),
@@ -63,3 +63,48 @@ suite.addBatch({
     condition: undefined, message: 'hogehoge',
   }, false),
 }).export(module);
+
+
+vows.describe('EventEmitter tests').addBatch({
+  'create extended `EventEmitter`': {
+    topic: function () { 
+      var wrap = {};
+      wrap.emitter = new EventEmitter();
+      return wrap;
+    },
+    'should create `EventEmitter` object': function (topic) {
+      assert.instanceOf(topic.emitter, EventEmitter);
+    },
+    'call `soonEmit`': {
+      topic: function (parent) {
+        var emitter = parent.emitter;
+        emitter.soonEmit('success');
+        emitter.soonEmit('end', 1);
+        emitter.soonEmit('connect', 'hello', {});
+        return emitter;
+      },
+      on: {
+        'success': {
+          'should be `undefined`': function (arg1) {
+            assert.isUndefined(arg1);
+          }
+        },
+        'end': {
+          'should be `1`': function (arg1) {
+            assert.equal(arg1, 1);
+          }
+        },
+        'connect': {
+          'first argument should be `hello`': function (arg1) {
+            assert.equal(arg1, 'hello');
+          },
+          'second argument should be `{}`': function (err, arg1, arg2) {
+            console.log(arg1, arg2);
+            assert.isEmpty(arg2);
+          }
+        },
+      }
+    },
+  }
+}).export(module);
+
